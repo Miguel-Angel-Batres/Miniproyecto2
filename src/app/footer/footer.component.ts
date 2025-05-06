670a043b16fac538a839efa53aff92736e6b1b29
ArielParra <arielemilio_parra@hotmail.com>
2025-05-06 12:56:40 -0600

    footer sec dom y favicon

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { SecuredomPipe } from '../securedom.pipe';
 @Component({
   selector: 'app-footer',
-  imports: [RouterModule],
+  imports: [RouterModule, SecuredomPipe],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  address: string = 'Calle Falsa 123, CDMX';
+  phone: string = '+52 55 1234 5678';
+  email: string = 'contacto@SantaCruzGYM.com';
 }
