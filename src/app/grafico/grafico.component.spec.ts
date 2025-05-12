3d3ff3210f84c82d3b150a3baf96f5bceadedb0b
pepe_el_pollo <al350553@edu.uaa.mx>
2025-05-12 10:34:24 -0600

    Add chart.js and ng2-charts dependencies; remove contacto component and update routes; implement GraficoComponent for displaying statistics

--- src/app/grafico/grafico.component.spec.ts
+++ src/app/grafico/grafico.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ContactoComponent } from './contacto.component';
+import { GraficoComponent } from './grafico.component';
 
-describe('ContactoComponent', () => {
-  let component: ContactoComponent;
-  let fixture: ComponentFixture<ContactoComponent>;
+describe('GraficoComponent', () => {
+  let component: GraficoComponent;
+  let fixture: ComponentFixture<GraficoComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ContactoComponent]
+      imports: [GraficoComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(ContactoComponent);
+    fixture = TestBed.createComponent(GraficoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
