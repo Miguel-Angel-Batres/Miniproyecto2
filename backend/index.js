c0208e818a9bb9f7ad09c7752b085175f277e69e
pepe_el_pollo <al350553@edu.uaa.mx>
2025-06-05 12:04:30 -0600

    fix: update API URLs to use production environment

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const app=require('./app');
 require('./firebase');
 app.listen(3000, () => {
-    console.log('Servidor escuchando en http://localhost:3000');
+    console.log('Servidor escuchando en https://gimnasio-santa-cruz-ww7d.onrender.com');
   });
