20d2238af85779573fa554a6a30192d4749b553f
pepe_el_pollo <al350553@edu.uaa.mx>
2025-06-05 11:06:04 -0600

    fix: add console logs for environment variables in generate-env.js

--- scripts/generate-env.js
+++ scripts/generate-env.js
@@ -17,6 +17,18 @@ export const recaptchav2Config = {
 };
 `;
 
+console.log('Variables de entorno cargadas:');
+console.log({
+  FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
+  RECAPTCHA_SITE_KEY: process.env.RECAPTCHA_SITE_KEY,
+});
+
 fs.writeFileSync(
   path.resolve(__dirname, '../src/env.ts'),
   fileContent.trim(),
