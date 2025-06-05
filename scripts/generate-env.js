7fff19962f40e7e67bdcdda01661968abff59840
pepe_el_pollo <al350553@edu.uaa.mx>
2025-06-05 10:53:23 -0600

    fix: update environment variable references in generate-env.js

--- scripts/generate-env.js
+++ scripts/generate-env.js
@@ -3,17 +3,17 @@ const path = require('path');
 
 const fileContent = `
 export const firebaseConfig = {
-  apiKey: '${process.env.VITE_FIREBASE_API_KEY}',
-  authDomain: '${process.env.VITE_FIREBASE_AUTH_DOMAIN}',
-  projectId: '${process.env.VITE_FIREBASE_PROJECT_ID}',
-  storageBucket: '${process.env.VITE_FIREBASE_STORAGE_BUCKET}',
-  messagingSenderId: '${process.env.VITE_FIREBASE_MESSAGING_SENDER_ID}',
-  appId: '${process.env.VITE_FIREBASE_APP_ID}',
-  measurementId: '${process.env.VITE_FIREBASE_MEASUREMENT_ID}',
+  apiKey: '${process.env.FIREBASE_API_KEY}',
+  authDomain: '${process.env.FIREBASE_AUTH_DOMAIN}',
+  projectId: '${process.env.FIREBASE_PROJECT_ID}',
+  storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET}',
+  messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID}',
+  appId: '${process.env.FIREBASE_APP_ID}',
+  measurementId: '${process.env.FIREBASE_MEASUREMENT_ID}',
 };
 
 export const recaptchav2Config = {
-  v2SiteKey: '${process.env.VITE_RECAPTCHA_SITE_KEY}',
+  v2SiteKey: '${process.env.RECAPTCHA_SITE_KEY}',
 };
 `;
 
