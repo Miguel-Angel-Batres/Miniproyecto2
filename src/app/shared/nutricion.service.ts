1c5a8e3fe4817304bab52e079bf24cbbcdbff291
Chinchillas-T3CU3 <tecuechinchillas@gmail.com>
2025-06-05 14:48:50 -0600

    formulario prueba post

--- src/app/shared/nutricion.service.ts
+++ src/app/shared/nutricion.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PlanDieta } from '../models/dieta.model';
+import { Firestore, collection, addDoc } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NutricionService {
 
-  constructor() { }
+  constructor(private firestore: Firestore) {}
+
+  guardarDatosNutricion(datos: PlanDieta) {
+    const nutricionRef = collection(this.firestore, 'nutricion');
+    return addDoc(nutricionRef, datos);
+  }
 }
